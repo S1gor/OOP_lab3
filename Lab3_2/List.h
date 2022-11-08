f775b5f45befb27367843bc168c61bc7f9fa8d6f
Igor Semikin <i.semikin@list.ru>
2022-11-08 19:46:40 +0300

    Added list methods

--- Lab3_2/List.h
+++ Lab3_2/List.h
@@ -1,16 +1,70 @@
 #pragma once
 
-class List
+template <typename T> class List
 {
 private:
-	int data;
-	List* next = nullptr;
-	List* prev = nullptr;
+	struct Element
+	{
+		int data;
+		Element* next = nullptr;
+		Element* prev = nullptr;
+	};
+
+	Element* head = nullptr;
+	Element* tail = nullptr;
+	int size;
 
 public:
-	List();
-	~List();
 
-	void pushList(List& l, int data);
+	Element* findElement(int pos)
+	{
+		Element* current = head;
+		for (int i = 1; i <= pos; i++)
+			current = current->next;
+		return current;
+	}
+
+	void pushList(int pos, int data)
+	{
+		Element* tmp = new Element;
+		tmp->data = data;
+
+		if (pos >= size)
+		{
+			tail = tmp;
+			tmp->next = nullptr;
+		}
+		else
+		{
+			tmp->next = findElement(pos);
+			tmp->next->prev = tmp;
+		}
+
+		if (pos == 0)
+		{
+			tmp->prev = nullptr;
+			head = tmp;
+		}
+		else if (size != 0)
+		{
+			tmp->prev = findElement(pos - 1);
+			tmp->prev->next = tmp;
+		}
+		size++;
+	}
+
+	void pushList(int data)
+	{
+		pushList(size, data);
+	}
+
+	void print()
+	{
+		if (head == nullptr)
+			return;
+
+		for (Element* cur = head; cur != nullptr; cur = cur->next)
+			printf("%d ", cur->data);
+	}
 
 };
--- Lab3_2/Main.cpp
+++ Lab3_2/Main.cpp
@@ -3,8 +3,11 @@
 
 int main()
 {
-
-
+	List<int>a;
+	a.pushList(10);
+	a.pushList(20);
+	a.pushList(1, 70);
+	a.print();
 
 
 	return 0;
